c0e17097dfdeb2b42410e4f4528076e43b2fd02c
Jose Ramón Cajide <jose.ramon.cajide@gmail.com>
2015-11-14 13:35:49 +0100

    Updated

--- scripts/modelwithmaxengines.sh
+++ scripts/modelwithmaxengines.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-
 #Fisrt argument is the dataset (optd_aircraft.csv)
 #Example: ./modelwithmaxengines.sh ../data/optd_aircraft.csv
 
-
-#Obtener el avión con el mayor número de motores
+#Get the airplane with maximun number of engines from the dataset
 csvsort -r -d '^' -c nb_engines $1 | uniq | uniq -c | head  | csvcut  -c model | head -2 | tail -n +2
 
-#Alternativa: csvsort -r -d '^' -c nb_engines optd_aircraft.csv | head -2 | csvcut  -c model | tail -1
+#Alternate method: csvsort -r -d '^' -c nb_engines optd_aircraft.csv | head -2 | csvcut  -c model | tail -1
