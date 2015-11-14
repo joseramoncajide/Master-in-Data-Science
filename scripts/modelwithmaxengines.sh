a5605111670f286a7830df252a2072cae9c11416
Jose Ramón Cajide <jose.ramon.cajide@gmail.com>
2015-11-14 13:33:20 +0100

    Added support for custom datasets

--- scripts/modelwithmaxengines.sh
+++ scripts/modelwithmaxengines.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+
+#Fisrt argument is the dataset (optd_aircraft.csv)
+#Example: ./modelwithmaxengines.sh ../data/optd_aircraft.csv
+
+
 #Obtener el avión con el mayor número de motores
-csvsort -r -d '^' -c nb_engines optd_aircraft.csv | uniq | uniq -c | head  | csvcut  -c model | head -2 | tail -n +2
+csvsort -r -d '^' -c nb_engines $1 | uniq | uniq -c | head  | csvcut  -c model | head -2 | tail -n +2
 
 #Alternativa: csvsort -r -d '^' -c nb_engines optd_aircraft.csv | head -2 | csvcut  -c model | tail -1
