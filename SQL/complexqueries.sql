eba2c5918a9030d1b407e9946b7ed643341ba9ba
Jose Ramón Cajide <jose.ramon.cajide@gmail.com>
2015-11-27 19:20:27 +0100

    import

--- SQL/complexqueries.sql
+++ SQL/complexqueries.sql
@@ -1,4 +1,3 @@
-
 select name, "2char_code", airline_code_2c, flight_freq
 from optd_airlines
 left outer join ref_airline_nb_of_flights
@@ -11,7 +10,6 @@ left outer join ref_airline_nb_of_flights AS r
 on o."2char_code" = airline_code_2c
 order by flight_freq desc   limit 10;
 
-
 select airline_code_2c,name, flight_freq
 from ref_airline_nb_of_flights AS r
 right outer join optd_airlines AS o
@@ -22,13 +20,4 @@ select name, country, elevation
 from optd_por_public
 where elevation > (select avg(elevation) from optd_por_public)
 limit 10;
-
-#csvgrep -d '^' optd_por_public.csv -c moddate -r '[0-9]{4}-[0-9]{2}-[0-9]{2}' > optd_clean.csv
-copy optd_por_public from '/tmp/optd_clean.csv' delimiter ',' csv header;
-
-csvsql -d '^' optd_aircraft.csv 
-CREAR TABLE:
-csvsql -d '^' optd_aircraft.csv   -i postgresql | psql optd 
-psql optd
-copy optd_airlines from '/tmp/optd_airlines.csv' delimiter '^' csv header;
 
